03baf6f3bf22c7b23e30e746612f9f3b934ba449
Brad Davies <me@varbrad.dev>
2020-12-04 18:24:00 +0000

    Add some basic Go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module test
+
+go 1.15
+
+require (
+	github.com/go-playground/assert/v2 v2.0.1
+	github.com/stretchr/testify v1.6.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(HelloWorld())
+}
+
+// HelloWorld returns a Hello World string!
+func HelloWorld() string {
+	return "Hello, world!"
+}
