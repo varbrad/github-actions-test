be0d761cc4a853dfed302119b039618431651376
Brad Davies <me@varbrad.dev>
2020-12-06 20:30:04 +0000

    Pointers & errors

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.15
 require (
 	github.com/go-playground/assert v1.2.1
 	github.com/go-playground/assert/v2 v2.0.1
-	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/stretchr/testify v1.6.1
 )
---
+++ pointers/pointers_test.go
@@ -0,0 +1,39 @@
+package pointers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWallet(t *testing.T) {
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		expected := Bitcoin(10)
+		actual := wallet.Balance()
+
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+
+		expected := Bitcoin(10)
+		actual := wallet.Balance()
+
+		assert.Equal(t, expected, actual)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+
+		err := wallet.Withdraw(Bitcoin(15))
+
+		assert.Equal(t, Bitcoin(10), wallet.Balance())
+		assert.Error(t, err)
+	})
+}
